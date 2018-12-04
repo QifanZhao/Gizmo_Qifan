ad05b49b3d6951cb5844eb3f1d8c2d5f3603f27e
QifanZhao <45443161+QifanZhao@users.noreply.github.com>
2018-12-04 00:36:11 +0000

    Add files via upload

--- Crazy_Machine__1_The_Fan.ino
+++ Crazy_Machine__1_The_Fan.ino
@@ -6,10 +6,10 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define MIN_PULSE_WIDTH 1000
 #define MAX_PULSE_WIDTH 2350
 #define DEFAULT_PULSE_WIDTH 1500
-#define FREQUENCY 50
+#define FREQUENCY 50   // use pwm to drive the three servo motors
 
-int inputPin = 2;               
-int val = 0;                    
+int inputPin = 2;      // connect Hall sensor to the Pin 2            
+int val = 0;           // setup the initial status of hall sensor      
 uint8_t servonum = 1;
 
 void setup() 
@@ -22,15 +22,15 @@ pwm.setPWMFreq(FREQUENCY);
 int pulseWidth(int angle)
 {
 int pulse_wide, analog_value;
-pulse_wide = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+pulse_wide = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH); // set the angle 
 analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
 Serial.println(analog_value);
 return analog_value;
 }
 
 void loop() {
-  val = digitalRead(inputPin);
-  if(val == HIGH) {
+  val = digitalRead(inputPin);   // use hall sensor to detect whether there is a phone
+  if(val == HIGH) {               
      pwm.setPWM(0, 0, pulseWidth(0));
      delay(200);
      pwm.setPWM(0, 0, pulseWidth(30));
@@ -46,7 +46,7 @@ void loop() {
      pwm.setPWM(4, 0, pulseWidth(45));
      delay(200);
      pwm.setPWM(4, 0, pulseWidth(0));
-     delay(200); // swipe 1
+     delay(200); //  status "swipe"
 
      pwm.setPWM(0, 0, pulseWidth(0));
      delay(200);
@@ -63,7 +63,7 @@ void loop() {
      pwm.setPWM(4, 0, pulseWidth(45));
      delay(200);
      pwm.setPWM(4, 0, pulseWidth(0));
-     delay(200); // swipe 2
+     delay(200); //  status "swipe"
 
      pwm.setPWM(0, 0, pulseWidth(0));
      delay(200);
@@ -80,7 +80,7 @@ void loop() {
      pwm.setPWM(4, 0, pulseWidth(45));
      delay(200);
      pwm.setPWM(4, 0, pulseWidth(0));
-     delay(200); // swipe 3
+     delay(200); //  status "swipe"
 
      pwm.setPWM(0, 0, pulseWidth(30));
      delay(200);
@@ -91,7 +91,7 @@ void loop() {
      pwm.setPWM(0, 0, pulseWidth(0));
      delay(200);
      pwm.setPWM(0, 0, pulseWidth(30));
-     delay(500); // double click
+     delay(500); //  status "double click" to like the post on the instagram
 
      pwm.setPWM(0, 0, pulseWidth(0));
      delay(200);
@@ -121,7 +121,7 @@ void loop() {
      pwm.setPWM(0, 0, pulseWidth(0));
      delay(200);
      pwm.setPWM(0, 0, pulseWidth(30));
-     delay(200); // hesitation
+     delay(200); //  status "hesitation"
 
      pwm.setPWM(0, 0, pulseWidth(0));
      delay(200);
@@ -223,7 +223,7 @@ void loop() {
     pwm.setPWM(4, 0, pulseWidth(45));
     delay(200);
     pwm.setPWM(4, 0, pulseWidth(0));
-    delay(200); // keep swiping
+    delay(200); // status "keep swiping"
     
   } else {
     pwm.setPWM(0, 0, pulseWidth(18));
@@ -277,7 +277,7 @@ void loop() {
     pwm.setPWM(0, 0, pulseWidth(5));
     delay(200);
     pwm.setPWM(0, 0, pulseWidth(0));
-    delay(200); // sleepy
+    delay(200); // status "sleepy"
    
     pwm.setPWM(3, 0, pulseWidth(0));
     delay(200);
@@ -306,7 +306,7 @@ void loop() {
     pwm.setPWM(3, 0, pulseWidth(0));
     delay(200);
     pwm.setPWM(3, 0, pulseWidth(60));
-    delay(500);// look around
+    delay(500);//  status "look around"
         
   }
 }
